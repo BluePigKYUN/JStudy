9150c6173cbf25263abcab7306c23bc3f1182075
BluePigKYUN <165987198+BluePigKYUN@users.noreply.github.com>
2024-08-12 17:15:22 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        
+        int sum1 = a + b + c;
+        int sum2 = a * a + b * b + c * c;
+        int sum3 = a * a * a + b * b * b + c * c * c;
+        
+        if(a == b && a == c){
+            answer = sum1 * sum2 * sum3;
+        } else if(a == b || a == c || b == c) {
+            answer = sum1 * sum2;
+        } else {
+            answer = sum1;
+        }
+        
+        return answer;
+    }
+}
