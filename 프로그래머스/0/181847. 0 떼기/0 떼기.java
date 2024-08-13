55828eb6a826300c80f8f66eff18466d073bc460
BluePigKYUN <165987198+BluePigKYUN@users.noreply.github.com>
2024-08-13 23:01:06 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,5 +1,6 @@
 class Solution {
     public String solution(String n_str) {
-        return n_str.replaceFirst("^0+", "");
+        return Integer.toString(Integer.parseInt(n_str));
+        //return n_str.replaceFirst("^0+", "");
     }
 }
