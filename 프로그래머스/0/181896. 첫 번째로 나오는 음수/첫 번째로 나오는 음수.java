4bdf72cd44191d47d9371dfe1370d62ff321c32d
BluePigKYUN <165987198+BluePigKYUN@users.noreply.github.com>
2024-08-13 16:06:21 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        boolean flag = false;
+        
+        for(int i = 0 ; i < num_list.length ; i++){
+            if(num_list[i] < 0){
+                answer = i;
+                flag = true;
+                break;
+            }
+        }
+        
+        return flag ? answer : -1;
+    }
+}
