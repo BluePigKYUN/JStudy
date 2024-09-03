fed929b5b4bd7516ab19fa3d6eade568a260ac69
BluePigKYUN <165987198+BluePigKYUN@users.noreply.github.com>
2024-09-03 14:08:45 +0900

    [level 0] Title: A 강조하기, Time: 13.08 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        for(char ch : myString.toCharArray()){
+            if(ch == 'a' || ch == 'A'){
+                ch = 'A';
+            } else if(ch >= 65 && ch <= 92){
+                ch += 32;
+            }
+            answer += ch;
+        }
+        
+        return answer;
+    }
+}
