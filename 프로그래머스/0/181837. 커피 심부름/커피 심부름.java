61eebdafa44ff8fcc7101ec70e5e157932c71891
BluePigKYUN <165987198+BluePigKYUN@users.noreply.github.com>
2024-09-09 10:44:29 +0900

    [level 0] Title: 커피 심부름, Time: 0.58 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        
+        for(int i = 0 ; i < order.length ; i++){
+            if(order[i].indexOf("americano") > -1|| order[i].indexOf("anything") > -1){
+                answer += 4500;
+            } else{
+                answer += 5000;
+            }
+        }
+        
+        return answer;
+    }
+}
