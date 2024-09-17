a1416ec41c3b3840e51e42a7f77d42068c904170
BluePigKYUN <duckkyun333@gmail.com>
2024-09-17 21:09:46 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        
+        for(int idx : array){
+            if(idx > height){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
